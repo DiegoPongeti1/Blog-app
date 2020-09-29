bbf72e07e34375db210404f2f0f7e0531eb061d8
Diego Pongeti <diego-p43@hotmail.com>
2020-09-29 16:12:21 -0300

    blogApp

---
+++ models/Categoria.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const { schedulingPolicy } = require("cluster")
+const { truncate } = require("fs")
+const Schema = mongoose.Schema
+
+const Categoria = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+mongoose.model("categorias", Categoria)
